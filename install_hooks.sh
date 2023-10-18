33397bd62c32d4b5dfe36ed8b6b500c95033c154
kagasu <1202244+kagasu@users.noreply.github.com>
2023-10-19 03:32:30 +0900

    fix

--- install_hooks.sh
+++ install_hooks.sh
@@ -2,19 +2,19 @@
 
 if test -e /etc/libvirt/ && ! test -e /etc/libvirt/hooks;
 then
-   mkdir -p /etc/libvirt/hooks;
+    mkdir -p /etc/libvirt/hooks;
 fi
 if test -e /etc/libvirt/hooks/qemu;
 then
     mv /etc/libvirt/hooks/qemu /etc/libvirt/hooks/qemu_last_backup
 fi
-if test -e /usr/local/bin/vfio-startup;
+if test -e /bin/vfio-startup;
 then
-    mv /usr/local/bin/vfio-startup /usr/local/bin/vfio-startup.bkp
+    mv /bin/vfio-startup /bin/vfio-startup.bkp
 fi
-if test -e /usr/local/bin/vfio-teardown;
+if test -e /bin/vfio-teardown;
 then
-    mv /usr/local/bin/vfio-teardown /usr/local/bin/vfio-teardown.bkp
+    mv /bin/vfio-teardown /bin/vfio-teardown.bkp
 fi
 if test -e /etc/systemd/system/libvirt-nosleep@.service;
 then
@@ -22,10 +22,10 @@ then
 fi
 
 cp systemd-no-sleep/libvirt-nosleep@.service /etc/systemd/system/libvirt-nosleep@.service
-cp hooks/vfio-startup /usr/local/bin/vfio-startup
-cp hooks/vfio-teardown /usr/local/bin/vfio-teardown
+cp hooks/vfio-startup /bin/vfio-startup
+cp hooks/vfio-teardown /bin/vfio-teardown
 cp hooks/qemu /etc/libvirt/hooks/qemu
 
-chmod +x /usr/local/bin/vfio-startup
-chmod +x /usr/local/bin/vfio-teardown
+chmod +x /bin/vfio-startup
+chmod +x /bin/vfio-teardown
 chmod +x /etc/libvirt/hooks/qemu
